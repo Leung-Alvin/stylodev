9b33506b32657a5e4c805f99658772ce8b40e34a
Leung-Alvin <89597642+Leung-Alvin@users.noreply.github.com>
2024-02-12 20:03:59 -0800

    Can add and remove entries off of Postgres database but also read prints

--- lassdb.py
+++ lassdb.py
@@ -1,5 +1,9 @@
 import psycopg2
 import config
+import textprint
+import lass
+
+DIRECTORY = 'prints/survey'
 
 def connect(config):
     try:
@@ -9,44 +13,92 @@ def connect(config):
     except (psycopg2.DatabaseError, Exception) as error:
         print(error)
 
+def create_table(connection, table_sql):
+    try:
+        cursor = connection.cursor()
+        cursor.execute(table_sql)
+        connection.commit()
+        cursor.close()
+    except (psycopg2.DatabaseError, Exception) as error:
+        print(error)
+
+def list_tables(connection):
+    try:
+        cursor = connection.cursor()
+        cursor.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'")
+        tables = cursor.fetchall()
+        cursor.close()
+        return tables
+    except (psycopg2.DatabaseError, Exception) as error:
+        print(error)
+
+def drop_table(connection, table_name):
+    try:
+        cursor = connection.cursor()
+        cursor.execute("DROP TABLE " + table_name)
+        connection.commit()
+        cursor.close()
+    except (psycopg2.DatabaseError, Exception) as error:
+        print(error)
+
+def list_all_in_table(connection, table_name):
+    try:
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM " + table_name)
+        rows = cursor.fetchall()
+        cursor.close()
+        return rows
+    except (psycopg2.DatabaseError, Exception) as error:
+        print(error)
+
+def add_to_table(connection, table_name, username, text):
+    try:
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO " + table_name + " (username, text) VALUES (%s, %s)", (username, text))
+        connection.commit()
+        cursor.close()
+    except (psycopg2.DatabaseError, Exception) as error:
+        print(error)
+
+def formatted_table(connection, table_name):
+    try:
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM " + table_name)
+        rows = cursor.fetchall()
+        cursor.close()
+        return rows
+    except (psycopg2.DatabaseError, Exception) as error:
+        print(error)
+
+def formatted_print(connection, table_name):
+    contents = list_all_in_table(connection, table_name)
+    ret = ""
+    for content in contents:
+        ret += content[0] + "\n" + content[1] + "\n\n"
+    return ret
 
 if __name__ == '__main__':
     configurations = config.load_config()
     conn = connect(configurations)
     cursor = conn.cursor()
-    db_name = configurations['database']
-    table_sql = '''CREATE TABLE IF NOT EXISTS ''' + db_name + ''' (
+    # db_name = configurations['database']
+    
+    table_name = 'prints'
+    table_sql = '''CREATE TABLE IF NOT EXISTS ''' + table_name + ''' (
         id SERIAL PRIMARY KEY,
-        name TEXT NOT NULL,
-        price REAL NOT NULL
+        username TEXT NOT NULL,
+        text VARCHAR(20000) NOT NULL
     )'''
-    postgres_insert_query = ''' INSERT INTO ''' + db_name + ''' (publisher_id,
-    publisher_name, publisher_estd, publsiher_location, publsiher_type)
-    VALUES (%s,%s,%s,%s,%s)'''
-    record_to_insert = [(1, 'Packt', 1950,
-                        'chennai', 'books'),
-                        (22, 'Springer', 1950,
-                        'chennai', 'books'),
-                        (23, 'Springer', 1950,
-                        'chennai', 'articles'),
-                        (54, 'Oxford', 1950,
-                        'chennai', 'all'),
-                        (52, 'MIT', 1950,
-                        'chennai', 'books'),
-                        (10, 'RCT', 1992,
-                        'Mumbai', 'all'),
-                        (6, 'ICT', 1995,
-                        'Delhi', 'article'),
-                        (7, 'PICT', 1955,
-                        'Pune', 'article')
-                        ]
-    for i in record_to_insert:
-        cursor.execute(postgres_insert_query, i)
-        conn.commit()
-        count = cursor.rowcount
-    print(count, "Record inserted successfully \
-    into publisher table")
-    cursor.execute(table_sql)
+
+    create_table(conn, table_sql)
+
+    # files = textprint.all_files(DIRECTORY)
+    # for file in files:
+    #     paragraphs = lass.break_file_into_paragraphs(file)
+    #     for paragraph in paragraphs:
+    #         add_to_table(conn, table_name, lass.get_file_author(file), paragraph)
+
+    print(formatted_print(conn, table_name))
     conn.commit()
     cursor.close()
     conn.close()
